a087fb92c3945563bf40a048cc0cc330fc6aa2df
dg022 <dgeorg6@uwo.ca>
2022-11-30 15:23:24 -0500

    adding rectangle

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,13 @@
 
 import io from 'socket.io-client'
+import Canvas from './canvas'
 const socket = io("http://localhost:3000"); 
 function App() {
 
   socket.emit('new', "bang"); 
 
   return (
-    <div className="App">
-      <header className="App-header">
-        hello this is working
-      </header>
-    </div>
+        <Canvas/>
   );
 }
 
---
+++ client/src/canvas.js
@@ -0,0 +1,38 @@
+import React, { useRef, useEffect } from 'react'
+
+const Canvas = props => {
+  
+  const canvasRef = useRef(null)
+  
+  const draw = (ctx, frameCount) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    ctx.fillStyle = '#000000'
+    ctx.beginPath()
+    ctx.rect(10, 20, 150, 100);
+    ctx.fill()
+  }
+  
+  useEffect(() => {
+    
+    const canvas = canvasRef.current
+    const context = canvas.getContext('2d')
+    let frameCount = 0
+    let animationFrameId
+    
+    //Our draw came here
+    const render = () => {
+      frameCount++
+      draw(context, frameCount)
+      animationFrameId = window.requestAnimationFrame(render)
+    }
+    render()
+    
+    return () => {
+      window.cancelAnimationFrame(animationFrameId)
+    }
+  }, [draw])
+  
+  return <canvas ref={canvasRef} {...props}/>
+}
+
+export default Canvas
